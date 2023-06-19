8a1dc9d31ce88a9cf25e739e1239e3a1de54b87d
Kyla <canares.kyla@gmailcom>
2023-06-19 15:44:47 +1000

    cloned basic spotify react project

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { Container } from "react-bootstrap"
 
+const CLIENT_ID='8be4629a5522462f8887b91836311024'
+
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=8b945ef10ea24755b83ac50cede405a0&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+  `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
 
 export default function Login() {
   return (
