9be4e4b80c74612691f5b43b7d1a80ac03ce838b
Kyla <canares.kyla@gmailcom>
2023-07-02 17:55:51 +1000

    added search results visually

--- client/src/TrackSearchResult.tsx
+++ client/src/TrackSearchResult.tsx
@@ -1,21 +1,24 @@
-// import React from "react"
+import React from "react";
+import { MediaItem } from "./Types";
 
-// export default function TrackSearchResult({ track, chooseTrack }) {
-//   function handlePlay() {
-//     chooseTrack(track)
-//   }
-
-//   return (
-//     <div
-//       className="d-flex m-2 align-items-center"
-//       style={{ cursor: "pointer" }}
-//       onClick={handlePlay}
-//     >
-//       <img src={track.albumUrl} style={{ height: "64px", width: "64px" }} />
-//       <div className="ml-3">
-//         <div>{track.title}</div>
-//         <div className="text-muted">{track.artist}</div>
-//       </div>
-//     </div>
-//   )
-// }
+export default function MediaSearchResult({
+  media,
+  setMedia,
+}: {
+  media: MediaItem;
+  setMedia: React.Dispatch<React.SetStateAction<MediaItem | undefined>>;
+}) {
+  return (
+    <div
+      className="d-flex m-2 align-items-center"
+      style={{ cursor: "pointer" }}
+      onClick={() => setMedia(media)}
+    >
+      <img src={media.imageUrl} style={{ height: "64px", width: "64px" }} />
+      <div className="ml-3">
+        <div>{media.title}</div>
+        <div className="text-muted">{media.artist}</div>
+      </div>
+    </div>
+  );
+}
