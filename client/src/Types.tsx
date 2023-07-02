7ee3bc46bf8849331d19a0f200024c80c73338ba
Kyla <canares.kyla@gmailcom>
2023-07-02 17:56:02 +1000

    added types

---
+++ client/src/Types.tsx
@@ -0,0 +1,16 @@
+export interface MediaItem {
+  title: string;
+  artist: string;
+  uri: string;
+  imageUrl: string;
+}
+
+export interface MediaState {
+  media: undefined | MediaItem;
+  setMedia: React.Dispatch<React.SetStateAction<MediaItem | undefined>>;
+}
+
+export enum MediaType {
+  EPISODE,
+  PLAYLIST,
+}
