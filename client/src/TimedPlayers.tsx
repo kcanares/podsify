e3b1f904cf0c532761e4da95352dc007c5afe8a7
Kyla Canares <kcanares@atlassian.com>
2023-08-06 18:43:13 +1000

    added functionality to switch between episode and songs

--- client/src/TimedPlayers.tsx
+++ client/src/TimedPlayers.tsx
@@ -1,6 +1,29 @@
 import { useState, useEffect, useContext } from "react";
 import { MediaItem } from "./Types";
 import SpotifyApiContext from "./SpotifyApiContext";
+import SpotifyWebApi from "spotify-web-api-node";
+
+async function addTracksToQueue(spotifyApi: SpotifyWebApi, tracks: Track[]) {
+  tracks.map(async (track) => {
+    try {
+      await spotifyApi.addToQueue(track.uri);
+    } catch (e) {
+      console.error(e);
+    }
+  });
+}
+
+function getTracksDuration(tracks: Track[]) {
+  return tracks.reduce(
+    (currDuration, nextTrack) => currDuration + nextTrack.duration,
+    0
+  );
+}
+
+type Track = {
+  uri: string;
+  duration: number;
+};
 
 export const TimedPlayers = ({
   episode,
@@ -12,10 +35,11 @@ export const TimedPlayers = ({
   const [isEpisodePlaying, setIsEpisodePlaying] = useState<boolean>(true);
   const spotifyApi = useContext(SpotifyApiContext);
   const episodeIntervalLength = 10000;
-  const songsIntervalCount = 3;
+  const songIntervalLength = 1;
   const [episodePosition, setEpisodePosition] = useState<number>(0); // where playback has paused and should start in the next interval
   const [playlistPosition, setPlaylistPosition] = useState<number>(0);
-  const [playlistTracks, setPlaylistTracks] = useState<string[]>();
+  const [playlistTracks, setPlaylistTracks] = useState<Track[]>([]);
+  const [intervalDuration, setIntervalDuration] = useState<number>(0);
 
   async function requestNextPlaylistTrackUris(
     playlistPosition: number,
@@ -25,42 +49,44 @@ export const TimedPlayers = ({
       offset: playlistPosition,
       limit: songsIntervalCount,
     });
-    const trackUris: string[] = res.body.items.flatMap(
-      (item) => item.track?.uri || []
+    const trackUris: Track[] = res.body.items.flatMap((item) =>
+      item.track === undefined || item.track === null
+        ? []
+        : {
+            uri: item.track.uri,
+            duration: item.track.duration_ms,
+          }
     );
 
     setPlaylistTracks(trackUris);
   }
 
   useEffect(() => {
-    const timer = setInterval(
-      () => setIsEpisodePlaying((prevState) => !prevState),
-      episodeIntervalLength
-    );
-
     if (isEpisodePlaying) {
       spotifyApi.play({
         uris: [episode.uri],
         position_ms: episodePosition,
       });
-      // add songs to queue
-      requestNextPlaylistTrackUris(playlistPosition, songsIntervalCount);
-      // calculate how long these episodes will take
-    } else {
+      setTimeout(() => setIsEpisodePlaying(false), episodeIntervalLength);
       setEpisodePosition(episodePosition + episodeIntervalLength);
-      // play next items on queue
-      // set interval to queue length
-      spotifyApi.play({
-        context_uri: playlist.uri,
-        // offset: { position: episodePosition },
-      });
+    } else {
+      // add songs to queue
+      requestNextPlaylistTrackUris(playlistPosition, songIntervalLength);
+      if (playlistTracks != undefined)
+        addTracksToQueue(spotifyApi, playlistTracks);
+      else console.error("playlistTracks are undefined");
+
+      spotifyApi.skipToNext();
+      const tracksDuration = getTracksDuration(playlistTracks);
+      console.log(tracksDuration);
+      setTimeout(() => {
+        setIsEpisodePlaying(true);
+      }, tracksDuration);
+      setPlaylistPosition(playlistPosition + songIntervalLength);
     }
 
-    // Clean up the timer when the component unmounts
-    return () => {
-      clearInterval(timer);
-    };
-  }, [isEpisodePlaying, playlistTracks]);
+    return () => {};
+  }, [isEpisodePlaying]);
 
   return isEpisodePlaying ? <p>episode</p> : <p>songs</p>;
 };
