8b54a4ec9e5711c84fee6fd01b8c7551dbaf5de8
Kyla <canares.kyla@gmailcom>
2023-12-03 13:17:22 +1100

    referenced new env variables

--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 
 const CLIENT_ID = "8be4629a5522462f8887b91836311024";
 
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${process.env.REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
 export default function Login() {
   return (
